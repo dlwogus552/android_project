14ca0e192a8ae536acb92ca1d9b9e742251135f9
momocora <delispice6@gmail.com>
2024-02-06 16:59:06 +0900

    hospital 수정중

--- app/src/main/java/org/third/medicalapp/hospital/model/Hospital.kt
+++ app/src/main/java/org/third/medicalapp/hospital/model/Hospital.kt
@@ -1,11 +1,11 @@
 package org.third.medicalapp.hospital.model
 
 data class Hospital(
-    var h_name: String,
-    var h_code: String,
+    var hname: String,
+    var hcode: String,
     var addr:String,
     var city: String,
-    var si_gun: String,
+    var sigun: String,
     var dong: String,
     var x: Double,
     var y: Double,
