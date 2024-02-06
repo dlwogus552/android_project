8192aa0733162d4c1412a39ce2488e615b91a68d
dlwogus552 <145645760+dlwogus552@users.noreply.github.com>
2024-02-06 09:53:42 +0900

    fregment

---
+++ app/src/main/java/org/third/medicalapp/user/MyPageFragment.kt
@@ -0,0 +1,36 @@
+package org.third.medicalapp.user
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.lifecycle.ViewModelProvider
+import org.third.medicalapp.R
+import org.third.medicalapp.databinding.FragmentMyPageBinding
+
+
+class MyPageFragment : Fragment() {
+
+    private var _binding: FragmentMyPageBinding? = null
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+
+        _binding = FragmentMyPageBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+        return root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
---
+++ app/src/main/java/org/third/medicalapp/user/UserWriteFragment.kt
@@ -0,0 +1,42 @@
+package org.third.medicalapp.user
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import org.third.medicalapp.databinding.FragmentUserWriteBinding
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [UserWriteFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class UserWriteFragment : Fragment() {
+
+    private var _binding: FragmentUserWriteBinding? = null
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+
+        _binding = FragmentUserWriteBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+        return root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
