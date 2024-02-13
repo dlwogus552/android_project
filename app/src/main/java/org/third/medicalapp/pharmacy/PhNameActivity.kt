9910a607a7babbb0021154f2be5c98894b202d0d
momocora <delispice6@gmail.com>
2024-02-13 20:03:43 +0900

    병원 약국 이름으로 검색기능 추가

--- app/src/main/java/org/third/medicalapp/pharmacy/PhNameActivity.kt
+++ app/src/main/java/org/third/medicalapp/pharmacy/PhNameActivity.kt
@@ -4,28 +4,25 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import org.third.medicalapp.R
-import org.third.medicalapp.databinding.ActivityPhLocalBinding
+import org.third.medicalapp.databinding.ActivityPhNameBinding
 
-class PhLocalActivity : AppCompatActivity() {
-    lateinit var binding: ActivityPhLocalBinding
+class PhNameActivity : AppCompatActivity() {
+    lateinit var binding: ActivityPhNameBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityPhLocalBinding.inflate(layoutInflater)
+        binding = ActivityPhNameBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
         val intent = Intent(this, PharmacyListActivity::class.java)
-
         binding.btnCloseX.setOnClickListener {
             startActivity(intent)
             finish()
         }
 
-        binding.btnDongSearchPh.setOnClickListener {
-            val dong = binding.tvDongPh.text.toString()
-            intent.putExtra("dong", dong)
+        binding.btnPharmacySearch.setOnClickListener {
+            val pharmacy = binding.tvPharmacySearch.text.toString()
+            intent.putExtra("pharmacy", pharmacy)
             startActivity(intent)
             finish()
         }
-
     }
 }
--- app/src/main/java/org/third/medicalapp/pharmacy/PharmacyListActivity.kt
+++ app/src/main/java/org/third/medicalapp/pharmacy/PharmacyListActivity.kt
@@ -13,6 +13,7 @@ import org.third.medicalapp.R
 import org.third.medicalapp.databinding.ActivityPharmacyListBinding
 import org.third.medicalapp.hospital.NameSearchActivity
 import org.third.medicalapp.pharmacy.apater.PharmacyAdapter
+import org.third.medicalapp.pharmacy.model.Pharmacy
 import org.third.medicalapp.pharmacy.model.PharmacyList
 import org.third.medicalapp.util.MyApplication
 import retrofit2.Call
@@ -29,18 +30,23 @@ class PharmacyListActivity : AppCompatActivity() {
         supportActionBar?.title = "약국 찾기"
 
         binding.btnLocalSelect.setOnClickListener {
-
+            val intent = Intent(this, PhLocalActivity::class.java)
+            startActivity(intent)
+            finish()
         }
 
         binding.btnNameSelect.setOnClickListener {
+            val intent = Intent(this, PhNameActivity::class.java)
+            startActivity(intent)
+            finish()
 
         }
 
-        if(MyApplication.checkAdmin()){
-            binding.addFab.visibility= View.VISIBLE
+        if (MyApplication.checkAdmin()) {
+            binding.addFab.visibility = View.VISIBLE
         }
-        binding.addFab.setOnClickListener{
-            startActivity(Intent(this,PharmacyAddActivity::class.java))
+        binding.addFab.setOnClickListener {
+            startActivity(Intent(this, PharmacyAddActivity::class.java))
         }
     }
 
@@ -60,30 +66,88 @@ class PharmacyListActivity : AppCompatActivity() {
 
     override fun onStart() {
         super.onStart()
+        val Pharmacy = intent.getStringExtra("pharmacy")
+        val Dong = intent.getStringExtra("dong")
+
         val networkService = (applicationContext as MyApplication).pharmacyService
 //        전체 약국호출
-        val pharmacyListCall = networkService.doGetPharmacyList()
-        pharmacyListCall.enqueue(object : retrofit2.Callback<PharmacyList> {
-            override fun onResponse(call: Call<PharmacyList>, response: Response<PharmacyList>) {
-                if (response.isSuccessful) {
+        if (Dong == null && Pharmacy == null) {
+            val pharmacyListCall = networkService.doGetPharmacyList()
+            pharmacyListCall.enqueue(object : retrofit2.Callback<PharmacyList> {
+                override fun onResponse(
+                    call: Call<PharmacyList>,
+                    response: Response<PharmacyList>
+                ) {
+                    if (response.isSuccessful) {
+                        binding.recyclerListViewPH.layoutManager =
+                            LinearLayoutManager(this@PharmacyListActivity)
+                        val pharmacy = response.body()?.pharmacyList
+                        val adapter = PharmacyAdapter(this@PharmacyListActivity, pharmacy)
+                        binding.recyclerListViewPH.adapter = adapter
+                        binding.recyclerListViewPH.addItemDecoration(
+                            DividerItemDecoration(
+                                this@PharmacyListActivity,
+                                LinearLayoutManager.VERTICAL
+                            )
+                        )
+                    }
+                }
+
+                override fun onFailure(call: Call<PharmacyList>, t: Throwable) {
+                    call.cancel()
+                }
+
+            })
+        }
+        //이름으로 검색
+        if (Pharmacy != null) {
+            val pharmacyNameCall = networkService.doGetPharmacyName(pharmacy = Pharmacy)
+            pharmacyNameCall.enqueue(object : retrofit2.Callback<PharmacyList> {
+                override fun onResponse(
+                    call: Call<PharmacyList>,
+                    response: Response<PharmacyList>
+                ) {
                     binding.recyclerListViewPH.layoutManager =
                         LinearLayoutManager(this@PharmacyListActivity)
-                    val pharmacy = response.body()?.pharmacyList
-                    val adapter = PharmacyAdapter(this@PharmacyListActivity, pharmacy)
+                    val pharmacyName = response.body()?.pharmacyList
+                    val adapter = PharmacyAdapter(this@PharmacyListActivity, pharmacyName)
                     binding.recyclerListViewPH.adapter = adapter
                     binding.recyclerListViewPH.addItemDecoration(
                         DividerItemDecoration(
-                            this@PharmacyListActivity,
-                            LinearLayoutManager.VERTICAL
+                            this@PharmacyListActivity, LinearLayoutManager.VERTICAL
                         )
                     )
                 }
-            }
 
-            override fun onFailure(call: Call<PharmacyList>, t: Throwable) {
-                call.cancel()
-            }
+                override fun onFailure(call: Call<PharmacyList>, t: Throwable) {
+                    call.cancel()
+                }
 
-        })
+            })
+        }
+        //지역검색
+        if(Dong != null){
+            val dongListCall = networkService.doGetDong(dong = Dong)
+            dongListCall.enqueue(object : retrofit2.Callback<PharmacyList>{
+                override fun onResponse(
+                    call: Call<PharmacyList>,
+                    response: Response<PharmacyList>
+                ) {
+                    if (response.isSuccessful){
+                        binding.recyclerListViewPH.layoutManager=
+                            LinearLayoutManager(this@PharmacyListActivity)
+                        val dongPharmacy = response.body()?.pharmacyList
+                        val adapter = PharmacyAdapter(this@PharmacyListActivity, dongPharmacy)
+                        binding.recyclerListViewPH.adapter =adapter
+                        binding.recyclerListViewPH.addItemDecoration(DividerItemDecoration(this@PharmacyListActivity, LinearLayoutManager.VERTICAL))
+                    }
+                }
+
+                override fun onFailure(call: Call<PharmacyList>, t: Throwable) {
+                    call.cancel()
+                }
+
+            })
+        }
     }
 }
